d1dc3fad97d70669676bba1347088da673608dc7
FelixTheC <felixeisenmenger@gmx.net>
2020-06-07 18:11:56 +0200

    [CRE] a cached set class with memory limit

---
+++ strongtyping/cached_set.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@created: 07.06.20
+@author: felix
+"""
+import sys
+from typing import Union
+
+
+class CachedSet(set):
+    """
+    Warning only use for caching when Memory limit is reached all items will be cleared
+    """
+
+    def __init__(self, memory_limit: Union[int, float] = 1, *args, **kwargs):
+        """
+        :param memory_limit: in MB
+        """
+        self.memory_limit = memory_limit * 1000000
+        super(CachedSet, self).__init__(*args, **kwargs)
+
+    def add(self, element: any) -> None:
+        if sys.getsizeof(self) > self.memory_limit:
+            self.clear()
+        super().add(element)
