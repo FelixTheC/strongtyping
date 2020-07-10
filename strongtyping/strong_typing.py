7449fcf01641928f8e602b552194473bcc6a44b3
FelixTheC <felixeisenmenger@gmx.net>
2020-07-10 08:35:07 +0200

    [FIX] circular import error

--- strongtyping/strong_typing.py
+++ strongtyping/strong_typing.py
@@ -17,7 +17,6 @@ from typing import Any
 from typing import TypeVar
 
 from strongtyping.cached_set import CachedSet
-import strongtyping.easy_property
 
 
 class TypeMisMatch(AttributeError):
@@ -258,10 +257,38 @@ class match_class_typing:
         return cls(*args, **kwargs)
 
 
+def action(f, frefs):
+
+    """
+    This code is original from Ruud van der Ham https://github.com/salabim/easy_property
+    """
+    if f.__qualname__ == action.qualname:
+        if any(action.f[fref] is not None for fref in frefs.split("_")):
+            raise AttributeError(f"decorator defined twice")
+    else:
+        action.f.update({}.fromkeys(action.f, None))  # reset all values to None
+        action.qualname = f.__qualname__
+    action.f.update({}.fromkeys(frefs.split('_'), f))  # set all frefs values to f
+
+    # this line was added by myself
+    action.f['setter'] = match_typing(action.f['setter']) if action.f['setter'] is not None else None
+
+    return property(*(action.f[ref] if (ref != "documenter" or action.f[ref] is None)
+                      else action.f[ref](0) for ref in action.f))
+
+
+action.qualname = None
+action.f = dict.fromkeys(["getter", "setter", "deleter", "documenter"], None)
+
+
+def getter(func):
+    return action(func, 'getter')
+
+
 def setter(func):
-    return strongtyping.easy_property.action(func, 'setter')
+    return action(func, 'setter')
 
 
 def getter_setter(func):
-    return strongtyping.easy_property.action(func, 'getter_setter')
+    return action(func, 'getter_setter')
 
