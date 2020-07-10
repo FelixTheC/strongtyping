34212e40d4d47466b575772ee4ef5a3f302a17dd
FelixTheC <felixeisenmenger@gmx.net>
2020-07-10 08:38:44 +0200

    Develop (#35)
    * [UPD] test for user defined classes
    
    * [FIX] issues with py36
    
    * [FIX] issues with type in py36
    
    * [UPD] version
    
    * [UPD] allow exception to be None and display only RuntimeWarning (#25)
    
    * [UPD] version
    
    * [UPD] readme
    
    * [UPD] workflow for coverage badge
    
    * [UPD] requirements for workflow for coverage badge
    
    * [UPD] grammer and fix style issues (#30)
    
    * [UPD] support typing.Literal
    
    * [UPD] support typing.Literal
    
    * [UPD] ignore test files during codecov
    
    * [CRE] class decorator
    
    * [CRE] property typechecking
    
    * [UPD] include easy_property
    
    * [UPD] more detailed information over included decorators
    
    * [UPD] more detailed information over included decorators
    
    * [UPD] missing link
    
    * [FIX] Import Error
    
    * [FIX] circular import error

--- strongtyping/strong_typing.py
+++ strongtyping/strong_typing.py
@@ -258,6 +258,29 @@ class match_class_typing:
         return cls(*args, **kwargs)
 
 
+def action(f, frefs):
+
+    """
+    This code is original from Ruud van der Ham https://github.com/salabim/easy_property
+    """
+    if f.__qualname__ == action.qualname:
+        if any(action.f[fref] is not None for fref in frefs.split("_")):
+            raise AttributeError(f"decorator defined twice")
+    else:
+        action.f.update({}.fromkeys(action.f, None))  # reset all values to None
+        action.qualname = f.__qualname__
+    action.f.update({}.fromkeys(frefs.split('_'), f))  # set all frefs values to f
+
+    # this line was added by myself
+    action.f['setter'] = match_typing(action.f['setter']) if action.f['setter'] is not None else None
+
+    return property(*(action.f[ref] if (ref != "documenter" or action.f[ref] is None)
+                      else action.f[ref](0) for ref in action.f))
+
+action.qualname = None
+action.f = dict.fromkeys(["getter", "setter", "deleter", "documenter"], None)
+
+
 def setter(func):
     return action(func, 'setter')
 
