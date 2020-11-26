9bb784188fd7a3c69270e74c4bbc39c155f35f00
FelixTheC <felixeisenmenger@gmx.net>
2020-11-26 21:08:45 +0100

    [UPD] create own install function

--- strongtyping/__init__.py
+++ strongtyping/__init__.py
@@ -12,14 +12,3 @@ __all__ = ['_utils',
            'cached_set',
            'cached_dict',
            'type_namedtuple']
-
-import os
-
-try:
-    from strongtyping_modules.install import install
-except ImportError:
-    pass
-else:
-    if not bool(os.environ.get('ST_MODULES_INSTALLED', '0')):
-        install()
-        os.environ['ST_MODULES_INSTALLED'] = '1'
