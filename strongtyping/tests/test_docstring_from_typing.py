7ee6c24ab4dd4abdd583014d548c713439801008
felix <felixeisenmenger@gmx.net>
2021-05-16 21:46:44 +0200

    [UPD] separate test with py3.9 only syntax

--- strongtyping/tests/test_docstring_from_typing.py
+++ strongtyping/tests/test_docstring_from_typing.py
@@ -4,11 +4,16 @@
 @created: 16.05.21
 @author: felix
 """
-from typing import Tuple
+import decimal
+import sys
+from typing import Tuple, List, Union
+
+import pytest
 
 from strongtyping.docs_from_typing import rest_docs_from_typing, numpy_docs_from_typing
 
 
+@pytest.mark.skipif(sys.version_info.minor < 9, reason='"type" object subscribable available in py3.9')
 def test_rest_docstring_from_typing():
 
     @rest_docs_from_typing
@@ -25,6 +30,7 @@ def test_rest_docstring_from_typing():
     assert Foo.foo.__doc__
 
 
+@pytest.mark.skipif(sys.version_info.minor < 9, reason='"type" object subscribable available in py3.9')
 def test_numpy_docstring_from_typing():
 
     @numpy_docs_from_typing
@@ -40,3 +46,34 @@ def test_numpy_docstring_from_typing():
     assert foo.__doc__
     assert Foo.foo.__doc__
 
+
+def test_rest_docstring_from_typing_2():
+
+    @rest_docs_from_typing
+    def foo(val_a: int, val_b: List[Union[int, float, decimal.Decimal]], val_c: Tuple[str, ...], *args, **kwargs):
+        pass
+
+    class Foo:
+
+        @rest_docs_from_typing
+        def foo(self, val_a: int, val_b: list[str]):
+            pass
+
+    assert foo.__doc__
+    assert Foo.foo.__doc__
+
+
+def test_numpy_docstring_from_typing_2():
+
+    @numpy_docs_from_typing
+    def foo(val_a: int, val_b: List[Union[int, float, decimal.Decimal]], val_c: Tuple[str, ...], *args, **kwargs):
+        pass
+
+    class Foo:
+
+        @numpy_docs_from_typing
+        def foo(self, val_a: int, val_b: list[str]):
+            pass
+
+    assert foo.__doc__
+    assert Foo.foo.__doc__
