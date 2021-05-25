365416873ddc99d2da975b3233617068a0428a11
FelixTheC <felixeisenmenger@gmx.net>
2021-05-25 22:01:52 +0200

    [FEAT] add autogenerated stubs

---
+++ strongtyping/stubs/__init__.pyi
@@ -0,0 +1,8 @@
+# Names in __all__ with no definition:
+#   _utils
+#   cached_dict
+#   cached_set
+#   docstring_typing
+#   strong_typing
+#   strong_typing_utils
+#   type_namedtuple
---
+++ strongtyping/stubs/type_namedtuple.pyi
@@ -0,0 +1,8 @@
+from strongtyping.docstring_typing import check_doc_str_type as check_doc_str_type
+from strongtyping.strong_typing import check_type as check_type, match_typing as match_typing
+from typing import Any, List, Tuple, Union
+
+use_match_typing: Any
+
+def typed_namedtuple(typename: str, field_names: Union[List[str], str, List[Tuple[str, Any]]], *,
+                     rename: bool=..., defaults: Union[list, tuple]=..., module: str=...) -> Any: ...
