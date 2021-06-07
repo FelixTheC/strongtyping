a30895d4626a299a8f1b90931379f0c8c7376d83
eisenmenger <f.eisenmenger@micro-biolytics.com>
2021-06-07 15:57:29 +0200

    [FIX] mypy support

--- strongtyping-stubs/strong_typing.pyi
+++ strongtyping-stubs/strong_typing.pyi
@@ -1,18 +1,9 @@
-from typing import Any, Optional, Type
-
 from strongtyping._utils import action as action, remove_subclass as remove_subclass
 from strongtyping.cached_set import CachedSet as CachedSet
-from strongtyping.strong_typing_utils import TypeMisMatch as TypeMisMatch, check_type as check_type
+from strongtyping.strong_typing_utils import TypeMisMatch as TypeMisMatch, check_type as check_type, checking_typing_typedict_values as checking_typing_typedict_values, default_return_queue as default_return_queue, get_origins as get_origins, py_version as py_version
+from typing import Any, Optional, Type
 
-def match_typing(
-    _func: Any = ...,
-    *,
-    excep_raise: Type[Exception] = ...,
-    cache_size: Any = ...,
-    subclass: bool = ...,
-    severity: Any = ...,
-    **kwargs: Any,
-) -> Any: ...
+def match_typing(_func: Any=..., *, excep_raise: Type[Exception]=..., subclass: bool=..., severity: Any=..., **kwargs: Any) -> Any: ...
 
 class match_class_typing:
     def __new__(cls, instance: Optional[Any] = ..., *args: Any, **kwargs: Any): ...
@@ -24,6 +15,9 @@ class match_class_typing:
     def __getattr__(self, item: Any): ...
     @staticmethod
     def __has_annotations__(obj: Any): ...
+    @property
+    def is_typed_dict(self): ...
+    def create_error_msg(self, args: dict) -> Any: ...
     def __call__(self, *args: Any, **kwargs: Any): ...
 
 def getter(func: Any): ...
---
+++ strongtyping-stubs/types.pyi
@@ -0,0 +1,5 @@
+from strongtyping.strong_typing_utils import py_version as py_version
+from typing import Any
+
+Validator: Any
+IterValidator: Any
