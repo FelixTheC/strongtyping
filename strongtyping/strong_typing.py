d2312f3be0fdc3fc7a20f4e969ceb24ad2f6eb1a
FelixTheC <felixeisenmenger@gmx.net>
2022-12-22 11:21:23 +0100

    hotifx: not everyone uses `typing_extensions`

--- strongtyping/strong_typing.py
+++ strongtyping/strong_typing.py
@@ -4,7 +4,12 @@ import warnings
 from functools import wraps
 from typing import Type, get_args, get_origin
 
-from typing_extensions import NotRequired, Required
+try:
+    from typing_extensions import NotRequired, Required
+except ImportError:
+    NotRequired = object()
+    Required = object()
+
 
 from strongtyping._utils import _severity_level, action, remove_subclass
 from strongtyping.cached_set import CachedSet
