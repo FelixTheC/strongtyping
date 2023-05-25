bb40fe6c2633242c730c33e2cb1f553f429650f5
felix <felixeisenmenger@gmx.net>
2023-05-25 20:50:02 +0200

    chore: set correct required python version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ packages = find_packages(exclude=["test_*", "*.tests"])
 
 setup(
     name="strongtyping",
-    version="3.11.1",
+    version="3.11.2",
     description="Decorator which checks whether the function is called with the correct type of parameters",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -22,10 +22,9 @@ setup(
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],
     packages=packages,
-    python_requires=">=3.10",
+    python_requires=">=3.11",
     include_package_data=True,
 )
